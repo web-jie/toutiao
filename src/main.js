f767fcafc0db0f538b26075c56d7a04d9678b64b
yao <qqww@qq.com>
2020-03-26 14:24:43 +0800

    修改页面以及创建点击事件

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vant from 'vant'
+import axios from 'axios'
+//绑定到原型上边，可以使用this.$axios来调用。
+Vue.prototype.$axios = axios;
+
+Vue.use(Vant);
 
 Vue.config.productionTip = false
-Vue.use(Vant)
+
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,83 +1,84 @@
 <template>
-  <div class="total">
-    <div class="title"></div>
-    <div class="body">
+  <div class="container">
+    <div class="btn">
       <span class="iconfont iconicon-test"></span>
-      <span class="iconfont iconnew"></span>
-      <input type="text" placeholder="用户名/手机号码">
-      <input type="password" placeholder="密码">
-      <button class="btn">登录</button>
     </div>
-    <div class="base"></div>
+    <div class="logo">
+      <span class="iconfont iconnew"></span>
+    </div>
+    <div class="form">
+      <div class="form-user">
+        <input type="text" v-model="form.username" placeholder="用户名/手机号" />
+        <input type="password" v-model="form.password" placeholder="密码" />
+      </div>
+      <div class="form-btn">
+        <button @click="handleClick">登录</button>
+      </div>
+    </div>
   </div>
-  
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    handleClick(){
+      this.$axios({
+        url: "http://127.0.0.1:3000/login",
+        method: "POST",
+        data: this.form
 
-}
+      }).then(res=>{
+        const {message} = res.data;
+        this.$toast.success(message)
+      })
+    }
+  }
+};
 </script>
 
 <style lang='less'>
-.total{
-  
-  width: 360px;
-  height: 540px;
-  margin: 100px 100px;
-  background: #f3f2f3;
-  .title{
-    width: 100%;
-    height: 16px;
-    background: #747374;
+.container{
+  padding: 20 / 360 * 100vw;
+  .btn span{
+    font-size: 27 / 360 * 100vw;
+    line-height: 1;
   }
-  .body{
-    width: 100%;
-    height: 480px;
-    position: relative;
-    span{
-      display: block;
-    }
-    .iconicon-test{
-      font-size: 22px;
-      position: absolute;
-      top: 10px;
-      left: 12px;
-    }
-    .iconnew{
-      font-size: 120px;
-      text-align: center; 
-      color: #e63100;
-      padding-top: 100px;     
-    }
-    input{
-      display: block;
-      width: 230px;
-      height: 40px;
-      margin: 8px auto;
-      outline: none;
-      border: none;
-      border-bottom: 1px solid #747374;
-      background: #f3f2f3;
-    }
-    .btn{
-      display: block;
-      width: 250px;
-      height: 40px;
-      margin: 30px auto;
-      outline: none;
-      border: none;
-      border-radius: 20px;
-      background: #d93f00;
-      color: #fff;
-    }
-
+  .logo {
+    text-align: center;
+    margin-top: 20 /360 * 100vw;
   }
-  .base{
+  .logo span{
+    font-size: 126 / 360 *100vw;
+    color: #d81e06;
+  }
+  .form-user input {
     width: 100%;
-    height: 50px;
-    background: #010001;
+    height: 40 / 360 *100vw;
+    line-height: 40 / 360 *100vw;
+    outline: none;
+    border: none;
+    font-size: 18px;
+    margin-top: 20 /360 * 100vw;
+    border-bottom: 1px #999 solid;
+  }
+  .form-btn button{
+    width: 100%;
+    height: 48 /360 *100vw;
+    margin-top: 50 / 360 *100vw;
+    outline: none;
+    background: #d81e06;
+    color: #fff;
+    font-size: 18px;
+    border-radius: 25px;
+    border: none;
   }
 }
-
 </style>
