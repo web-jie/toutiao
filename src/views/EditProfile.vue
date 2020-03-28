1f020fb22b76b5ec3c4858628fe83aeed2f4589a
web-jie <864214989@qq.com>
2020-03-28 21:33:42 +0800

    头像上传已实现

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -1,61 +1,108 @@
 <template>
   <div>
-      <NavigateBar title="编辑资料" :showHome="false" />
-      <div class="avatar">
-        <img :src="$axios.defaults.baseURL + userInfo.head_img" >
-      </div>
-      <Listbar label="账号" :tips="userInfo.nickname" />
-      <Listbar label="密码" tips="******" />
-      <Listbar label="性别" :tips="['女','男'][userInfo.gender]" />
+    <NavigateBar title="编辑资料" :showHome="false" />
+    <div class="avatar">
+      <img :src="$axios.defaults.baseURL + userInfo.head_img" />
+      <van-uploader class="uploader" :after-read="afterRead" />
+    </div>
+    <Listbar label="账号" :tips="userInfo.nickname" />
+    <Listbar label="密码" tips="******" />
+    <Listbar label="性别" :tips="['女','男'][userInfo.gender]" />
   </div>
-
 </template>
 
 <script>
 //引入目录导航栏
-import Listbar from "@/components/Listbar.vue"
+import Listbar from "@/components/Listbar.vue";
 // 引入头部导航栏插件
-import NavigateBar from "@/components/NavigateBar.vue"
+import NavigateBar from "@/components/NavigateBar.vue";
 export default {
-  data(){
+  data() {
     return {
       userInfo: {},
-    }
+      //保存本地存储文件
+      userJson: {}
+    };
   },
   components: {
     NavigateBar,
-    Listbar,
+    Listbar
   },
-  mounted(){
-    const userJson = JSON.parse(localStorage.getItem('userInfo'));
-    console.log(userJson)
+  methods: {
+    afterRead(file) {
+      console.log(file);
+      const formdata = new FormData();
+      formdata.append("file", file.file);
+
+      this.$axios({
+        url: "/upload",
+        method: "POST",
+        headers: {
+          Authorization: this.userJson.token
+        },
+        data: formdata
+      }).then(res => {
+
+        const { url } = res.data.data;
+        this.userInfo.head_img = url;
+
+        //图片上传成功就要修改内容
+        this.handleEdit({
+          head_img: url
+        });
+      });
+    },
+    //创建一个修改数据的函数，data传递的数据可以是对象
+  handleEdit(data) {
+    this.$axios({
+      url: "/user_update/" + this.userInfo.id,
+      method: "POST",
+      headers: {
+        Authorization: this.userJson.token
+      },
+      data
+    }).then(res => {
+      this.$toast.success("头像修改成功");
+    });
+  }
+  },
+  mounted() {
+    const userJson = JSON.parse(localStorage.getItem("userInfo"));
+    this.userJson = userJson;
     this.$axios({
       url: "/user/" + userJson.user.id,
       //这个地方容易出错，建议直接复制，错误原因是header少写s
       headers: {
         Authorization: userJson.token
       }
-    }).then(res=>{
-
-      const {data} = res.data;
+    }).then(res => {
+      const { data } = res.data;
       this.userInfo = data;
-    })
-  }
+    });
+  },
+
+  
 };
 </script>
 
 <style lang="less" scoped>
-.avatar{
+.avatar {
   display: flex;
-  padding: 20/360*100vw;
+  padding: 20/360 * 100vw;
   justify-content: center;
   align-items: center;
   border-bottom: 5px #eee solid;
-  img{
-    width: 100/360*100vw;
-    height: 100/360*100vw;
+  position: relative;
+  img {
+    width: 100/360 * 100vw;
+    height: 100/360 * 100vw;
     border-radius: 50%;
   }
-
+  .uploader {
+    position: absolute;
+    opacity: 0;
+    width: 100/360 * 100vw;
+    height: 100/360 * 100vw;
+  }
 }
 </style>
