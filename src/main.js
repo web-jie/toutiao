8bb8471049ef4bb1941ee3191bdb5579696b2452
web-jie <864214989@qq.com>
2020-03-31 14:52:00 +0800

    路由拦截器

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import Vant from 'vant'
+import Vant, {Toast} from 'vant'
 import axios from 'axios'
 //绑定到原型上边，可以使用this.$axios来调用。
 Vue.prototype.$axios = axios;
@@ -21,6 +21,19 @@ router.beforeEach((to, from, next) => {
     next();
   }
 })
+// 路由拦截器
+axios.interceptors.response.use(response=>{
+  console.log(response)
+  return response;
+},error=>{
+  console.log(error.response)
+  const {statusCode,message} = error.response.data;
+  if(statusCode !== 400) return;
+  Toast.fail(message)
+
+  return Promise.reject(error.response.data)
+})
+
 
 new Vue({
   router,
