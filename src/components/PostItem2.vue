a420e1a19a9a15f573f09f1358ddef8e37fa7725
web-jie <864214989@qq.com>
2020-04-08 23:49:23 +0800

    修改详情页的跳转

--- src/components/PostItem2.vue
+++ src/components/PostItem2.vue
@@ -1,35 +1,40 @@
 <template>
-
-  <div class="imgList">
-     <h4>{{data.title}}</h4>
-     <div class="images">
-       <img v-for="(item, index) in data.cover" :key="index" :src="$axios.defaults.baseURL + item.url" alt="">
-     </div>
-     <p>{{data.user.nickname}} {{data.comment_length}}跟帖</p>
-  </div>
-    
+  <router-link :to="`/post/${data.id}`">
+    <div class="imgList">
+      <h4>{{data.title}}</h4>
+      <div class="images">
+        <img
+          v-for="(item, index) in data.cover"
+          :key="index"
+          :src="$axios.defaults.baseURL + item.url"
+          alt
+        />
+      </div>
+      <p>{{data.user.nickname}} {{data.comment_length}}跟帖</p>
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
-props:['data']
-}
+  props: ["data"]
+};
 </script>
 
 <style lang="less" scoped>
-.imgList{
-  padding: 0 20/360*100vw;
+.imgList {
+  padding: 0 20/360 * 100vw;
   border-bottom: 1px solid #999;
   margin: 10px 0;
-  .images{
+  .images {
     display: flex;
-    margin-top: 10/360*100vw;
-    img{
+    margin-top: 10/360 * 100vw;
+    img {
       width: 33%;
     }
   }
-  p{
-    margin: 10/360*100vw 0;
+  p {
+    margin: 10/360 * 100vw 0;
   }
 }
 </style>
