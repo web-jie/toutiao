bc222b94ba6ba6645247d2508ec9c6069db53361
web-jie <864214989@qq.com>
2020-04-09 20:32:18 +0800

    创建回复楼层静态

--- src/views/PostComment.vue
+++ src/views/PostComment.vue
@@ -18,6 +18,51 @@
         </div>
         <span class="reply">回复</span>
       </div>
+      <!-- 回复楼层 -->
+                  <div class="comment-floor">
+                <div class="floor-top">
+                    <div class="user">
+                        <!-- <span>1</span> -->
+                        <em>火星网友</em>
+                        <span>2小时前</span>
+                    </div>
+                    <span class="reply">回复</span>
+                </div>
+                <div class="content">
+                    文章评论的回复
+                </div>
+
+                <!-- 第二级 -->
+                <div class="comment-floor">
+                    <div class="floor-top">
+                        <div class="user">
+                            <!-- <span>2</span> -->
+                            <em>火星网友</em>
+                            <span>2小时前</span>
+                        </div>
+                        <span class="reply">回复</span>
+                    </div>
+                    <div class="content">
+                        文章评论的回复
+                    </div>
+
+                    <!-- 第3级 -->
+                    <div class="comment-floor">
+                        <div class="floor-top">
+                            <div class="user">
+                                <!-- <span>3</span> -->
+                                <em>火星网友</em>
+                                <span>2小时前</span>
+                            </div>
+                            <span class="reply">回复</span>
+                        </div>
+                        <div class="content">
+                            文章评论的回复
+                        </div>
+                    </div>
+                </div>
+            </div>
+
       <div class="content">文章说的很有道理！</div>
     </div>
   </div>
@@ -59,7 +104,31 @@ export default {
     }
   }
   .content{
-    margin: 15/360*100vw 0;
+    padding: 15/360*100vw 0;
+  }
+  .comment-floor{
+    padding: 0 2/360*100vw;
+    // border: 1px solid #000;
+    background: #e9e9e9;
+    .floor-top{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding-top: 10/360*100vw;
+      .user{
+        padding-left: 10/360*100vw;
+        span{
+          color: #999;
+          padding-left: 14/360*100vw;
+        }
+      }
+      .reply{
+        padding-right: 10/360*100vw;
+      }
+    }
+    .content{
+      padding-left: 10/360*100vw;
+    }
   }
 }
 </style>
